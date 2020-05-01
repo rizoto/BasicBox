bcdecb977d602b17c529824e3b5cae2e0fee12fe
Lubor Kolacny <lubor@itroja.com.au>
2020-05-01 19:45:12 +1000

    Initial Commit

---
+++ BasicBox/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BasicBox
+//
+//  Created by Lubor Kolacny on 1/5/20.
+//  Copyright © 2020 Lubor Kolacny. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
